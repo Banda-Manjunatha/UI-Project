5937a3db4ef019d879972bad184109ff94f83d30
Banda-Manjunatha <manjunathmaahi1255@gmail.com>
2024-04-24 20:23:03 +0530

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+import "./Styles.css";
+
+const App = () => {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+};
+
+export default App;
