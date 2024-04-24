6c16658d61ef7cec0a548fb45b7e2ce8d11d338e
Banda-Manjunatha <manjunathmaahi1255@gmail.com>
2024-04-24 22:31:41 +0530

    initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Dashboard from "./Dashboard";
 import "./Styles.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Dashboard />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 };
 
